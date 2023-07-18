36ceda2efb462bd53aa10d0fc3087e5fa8fca950
Nagesh Rathod <123568674+Nagesh-Rathod-29@users.noreply.github.com>
2023-07-18 19:31:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-MODEL_FILE_PATH = r"artifacts\\trained_model-1.0.0.pkl"
-VECTOR_FILE_PATH = r"artifacts\\trained_vector-1.0.0.pkl"
+MODEL_FILE_PATH = r"artifacts//trained_model-1.0.0.pkl"
+VECTOR_FILE_PATH = r"artifacts//trained_vector-1.0.0.pkl"
 VERSION = 1.0
 PORT = 8002
 HOST = '0.0.0.0'
